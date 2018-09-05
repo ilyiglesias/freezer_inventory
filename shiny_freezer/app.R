bf91b0fa6116997ac48cfa7d943a426ab541f053
Ilysa Iglesias <ilysa.iglesias@swc-iglesias-m.nmfs.local>
2018-09-05 15:34:20 -0700

    09/05/18: This project contains the freezer inventory spreadsheet, R code for producing maps of the remaining samples, and the code for creating a shiny app with this info. All data is now being backed up on git via freezer_inventory

---
+++ shiny_freezer/app.R
@@ -0,0 +1,48 @@
+
+library(shiny)
+library(leaflet)
+library(tidyverse)
+
+leaflet.tile <- "Esri.OceanBasemap"
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+   
+   # Application title
+   titlePanel("Groundfish Analysis Team Freezer Inventory"),
+   
+   # Sidebar with a select box for selecting a specific species  
+   sidebarLayout(
+      sidebarPanel(
+        selectInput("select", label = h3("Select species"), 
+          choices = inventory_sf$species, 
+          selected = 1)
+      ),
+      
+      # Show a plot of the generated distribution
+      mainPanel(
+         leafletOutput("mymap", height=1000)
+      )
+   )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+   
+  output$value <- renderPrint({ input$select })
+  
+   
+   output$mymap <- renderLeaflet({
+     m <- leaflet() %>% 
+       addProviderTiles(leaflet.tile) %>% 
+       addMarkers(data = filter(inventory_sf, species %in% input$select), popup = ~as.character(inv_no), label = ~as.character(year), clusterOptions = markerClusterOptions())
+       #setView(lng = -73.935242, lat= 40.730610, zoom = 10)
+     m
+     
+     
+   })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
